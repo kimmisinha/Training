eecc1bff95e823baae8c44d63c137450f6ad1c78
Kimmi Sinha <kimmi.sinha@newput.com>
2024-04-22 09:40:46 +0530

    'added media queries in assighment'

--- HTML_CSS_Assignment/js/sidebar.js
+++ HTML_CSS_Assignment/js/sidebar.js
@@ -1,8 +1,13 @@
+const sidebar = document.querySelector('.sidebar');
+
+// Function to toggle the active class
 function toggleSidebar() {
-  var sidebar = document.getElementById("sidebar");
-  if (sidebar.style.right === "0px") {
-      sidebar.style.right = "-250px";
-  } else {
-      sidebar.style.right = "0px";
-  }
+  sidebar.classList.toggle('active');
 }
+
+// Example event listener, like a button click, to trigger the toggle
+const toggleButton = document.querySelector('.filter-btn');
+const crossButton = document.querySelector('.cross');
+
+toggleButton.addEventListener('click', toggleSidebar);
+crossButton.addEventListener('click', toggleSidebar);
